c5044c91bf14cf2f47785fd3d48f7ad4bba1cd11
vishand <vishandr@gmail.com>
2022-08-08 11:07:26 +0300

    fetch list of monsters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(responce => responce.json())
+    .then((users) => this.setState(
+      () => {
+        return {monsters : users};
+    },
+    () => {
+      console.log(this.state)
+    }
+    ));
+  }
+
+//console.log(users))
+
+  render(){  
+    return (
+      <main>
+      {this.state.monsters.map((monster) => { 
+        return <h1 key={monster.id}>{monster.name}</h1> }
+        
+      )}
+      </main>
+    )
+  }
 }
+  ;
+
 
 export default App;
