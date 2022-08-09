a80c01270a23b9b90703b4fcc11f3c32bdbda7a3
vishand <vishandr@gmail.com>
2022-08-09 10:18:57 +0300

    add search box filtering monsters

--- src/App.js
+++ src/App.js
@@ -6,31 +6,49 @@ class App extends Component {
     super()
     this.state = {
       monsters: [],
+      filteredMonsters: [],
+      searchField: ''
     };
   }
-
+  
+  
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(responce => responce.json())
     .then((users) => this.setState(
       () => {
         return {monsters : users};
-    },
-    () => {
-      console.log(this.state)
+      }
+      ));
     }
-    ));
-  }
+    
+    onSearchChange = (event) => {
+      const searchField = event.target.value.toLocaleLowerCase();
+      this.setState(()=>{
+        return { searchField };
+      });
+    };
 
-//console.log(users))
 
-  render(){  
-    return (
+    render(){  
+    
+      const filteredMonsters = this.state.monsters.filter((monster)=>{
+        return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+
+      return (
       <main>
-      {this.state.monsters.map((monster) => { 
+      <input 
+        className='search-box' 
+        type='search' placeholder='search monsters'
+        onChange={ this.onSearchChange } />
+      
+      {/* {this.setState.filteredMonsters = this.state.monsters.filter((name) => {name.contains(this.state.search)})} */}
+
+      {filteredMonsters.map((monster) => { 
         return <h1 key={monster.id}>{monster.name}</h1> }
-        
-      )}
+        )}
+
       </main>
     )
   }
